e28414956728e52bed70065f5104a51cec5c878d
mkkjhn <mikko.uutela@student.oulu.fi>
2020-05-05 00:08:56 +0300

    initial commit, not working version

---
+++ app/src/main/java/com/example/nhltracker/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.nhltracker
+
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import kotlin.reflect.KCallable
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+
+// dynamic textview
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginTop
+import com.google.gson.JsonArray
+import com.google.gson.JsonParser
+import org.json.JSONArray
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get data from StartupActivity
+        val scheduleJSON = intent.getStringExtra("schedule")
+
+        // Display data
+        var gson = Gson()
+        var schedule = gson.fromJson(scheduleJSON, Schedule::class.java)
+
+
+        // denzil
+        //val scheduleJSON = intent.getStringExtra("schedule")
+        //val objects = listOf<Schedule>()
+
+        // creating TextView programmatically
+        val linearLayout = findViewById<LinearLayout>(R.id.linear_main)
+
+        val currentBGColor = R.color.dataTextBGColor2
+
+        for ( prop in Schedule::class.memberProperties) {
+            val itemJSON = prop
+            //println("${prop.name} = ${prop.get(schedule)}")
+
+            val textView = TextView(this)
+            //textView.gravity = Gravity.CENTER
+            textView.setTextColor(resources.getColor(R.color.dataTextColor))
+
+            val cd = textView.background as ColorDrawable
+
+            if ( cd.color == R.color.dataTextBGColor1) {
+                textView.setBackgroundColor(resources.getColor(R.color.dataTextBGColor2))
+            }
+            else {
+                textView.setBackgroundColor(resources.getColor(R.color.dataTextBGColor2))
+            }
+
+            textView.text = "${prop.name}: ${prop.get(schedule)}"
+            // Add TextView to LinearLayout
+            linearLayout?.addView(textView)
+
+        }
+        /*
+        val sList = listOf(schedule)
+
+        for (i in sList) {
+            val (name, value) = i
+            println("ITEROINTI ALKAA")
+            println(name)
+            println(value)
+        }
+        val (
+            copyright,
+            totalItems,
+            totalEvents,
+            totalGames,
+            totalMatches,
+            wait,
+            dates
+        ) = schedule
+        */
+
+
+
+        // Menee väärin, yksi kerrallaan alekkain, nyt ottaa ilmeisesti kaikki kerralla
+        /*
+        for ((a) in listOf(schedule)) {
+            val textView = TextView(this)
+            textView.layoutParams = ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            textView.gravity = Gravity.CENTER
+            textView.setTextColor(resources.getColor(R.color.dataTextColor))
+            textView.setBackgroundColor(resources.getColor(R.color.dataTextBGColor))
+            textView.text = "$a"
+            // Add TextView to LinearLayout
+            linearLayout?.addView(textView)
+        }
+        */
+
+        //textView.setOnClickListener { Toast.makeText(this@MainActivity, R.string.clicked_on_me, Toast.LENGTH_LONG).show() }
+
+    }
+}
+
+data class Schedule(
+    val copyright : String,
+    val totalItems : Int,
+    val totalEvents : Int,
+    val totalGames : Int,
+    val totalMatches : Int,
+    val wait : Int,
+    val dates : Array<String>
+)
+
+@Suppress("UNCHECKED_CAST")
+fun <R> readInstanceProperty(instance: Any, propertyName: String): R {
+    val property = instance::class.memberProperties
+        // don't cast here to <Any, R>, it would succeed silently
+        .first { it.name == propertyName } as KProperty1<Any, *>
+    // force a invalid cast exception if incorrect type here
+    return property.get(instance) as R
+}
---
+++ app/src/main/java/com/example/nhltracker/StartupActivity.kt
@@ -0,0 +1,98 @@
+package com.example.nhltracker
+
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import kotlinx.coroutines.*
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URL
+import android.widget.Toast
+
+class StartupActivity : AppCompatActivity() {
+
+    private var logo: ImageView? = null
+    private var text: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+
+        logo = findViewById<ImageView>(R.id.nhl_logo)
+        text = findViewById<TextView>(R.id.textAppName)
+
+        var resultJSON = ""
+        // Retrieve data from API
+        GlobalScope.launch { // launch a new coroutine in background and continue
+
+            try {
+                resultJSON =
+                    URL("https://statsapi.web.nhl.com/api/v1/schedule").readText()
+                //println(resultJSON) // print after delay
+            }
+            catch (e : Exception){
+                Toast.makeText(
+                    applicationContext, "data fetch failed", Toast.LENGTH_LONG).show()
+            }
+            delay(5500L) // non-blocking delay for 1 second (default time unit is ms)
+            // Switch activity to display data
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.putExtra("schedule", resultJSON)
+            startActivity(intent)
+        }
+
+        // Store data to a collection or DB
+        fadeAnimation()
+    }
+
+    private fun fadeAnimation() {
+        val fadeInLogo = ObjectAnimator.ofFloat(logo, View.ALPHA, 0.0f, 1.0f)
+        val fadeInText = ObjectAnimator.ofFloat(text, View.ALPHA, 0.0f, 1.0f)
+        val fadeOutLogo = ObjectAnimator.ofFloat(logo, View.ALPHA, 1.0f, 0.0f)
+        val fadeOutText = ObjectAnimator.ofFloat(text, View.ALPHA, 1.0f, 0.0f)
+
+        fadeInLogo.setDuration(2000)
+        fadeOutLogo.setDuration(2000)
+        fadeInText.setDuration(2000)
+        fadeOutText.setDuration(2000)
+        //fadeInLogo.repeatCount = 2
+        //fadeOutLogo.repeatCount = 2
+        //fadeInText.repeatCount = 2
+        //fadeOutText.repeatCount = 2
+
+        var anim = AnimatorSet()
+        var anim1 = AnimatorSet()
+        var anim2 = AnimatorSet()
+        //anim1.playTogether(fadeInLogo, fadeInText)
+        //anim2.playTogether(fadeOutLogo, fadeOutText)
+        anim1.play(fadeInLogo).with(fadeInText)
+        anim2.play(fadeOutLogo).with(fadeOutText)
+
+        //var animations = listOf<Animator>(anim, anim)
+        //anim.playSequentially(anim, anim)
+
+        anim.play(anim1).before(anim2)
+        anim.startDelay = 250
+        anim.start()
+
+        anim.addListener(object: Animator.AnimatorListener{
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                anim.start()
+                anim.removeAllListeners()
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+            }
+        })
+    }
+}
